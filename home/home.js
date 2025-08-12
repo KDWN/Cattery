cd434d0758142d38f22c00d095cf35d52f7607c1
KDWN <keiran.downsgts@gmail.com>
2025-08-12 22:27:35 +1200

    hamNav complete

--- home/home.js
+++ home/home.js
@@ -1,29 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("e")
-    let prevScrollPos = window.pageYOffset;
-    window.addEventListener("scroll", () => {
-        let currentScrollPos = window.pageYOffset;
-        let nav = document.getElementById('topNav');
-        let rect = document.getElementById('topNav').getBoundingClientRect();
-        
-        // user has scrolled up
-        if (prevScrollPos > currentScrollPos) {
-            nav.style.transform = 'translateY(0px)';
-            if ( Number(rect.top.toFixed() > 0 )) {
-                nav.style.transform = 'translateY(0px)'
-            }
-        }
-        
-        if (currentScrollPos >= 665) {
-            // user has scrolled down
-            if (prevScrollPos < currentScrollPos) {
-                nav.style.transform = 'translateY(-70px)';
-            }
-            if ( Number(rect.top.toFixed() < -70 )) {
-                nav.style.transform = 'translateY(-70px)'
-            }
-        }
-        // update previous scroll position
-        prevScrollPos = currentScrollPos;
-    }); 
+    console.log("Page Loaded");
+    
 });
