74897062d02cf83a4f7c2eac08075927faa92dd3
KDWN <keiran.downsgts@gmail.com>
2025-08-13 12:00:51 +1200

    clickham complete

--- home/home.js
+++ home/home.js
@@ -1,4 +1,28 @@
+function hamClick(menu) {
+    let nav = document.getElementById("topNav");
+    let icon0 = menu.childNodes[1];
+    let icon1 = menu.childNodes[3];
+    let icon2 = menu.childNodes[5];
+    if (nav.classList.contains("open")){
+        icon1.style.transform = '';
+        icon0.style.transform = '';
+        icon2.style.transform = '';
+        nav.classList.remove("open");
+    }
+    else {
+        icon1.style.transform = 'translate(-120px) scale(0.9)';
+        icon0.style.transform = 'translate(0, 18px) rotate(45deg) scale(0.8)';
+        icon2.style.transform = 'translate(0, -18px) rotate(-45deg) scale(0.8)';
+        nav.classList.add("open");
+    }
+}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Page Loaded");
-    
+    document.querySelector("#hamBtn").addEventListener( "click", function() {
+        console.log("clicked")
+        hamClick(this);
+    });
 });
